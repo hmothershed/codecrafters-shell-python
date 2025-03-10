334f7c4aa279d2960fb06bce5f2dc9fa2ceea505
hmothershed <harmony.mothershed@icloud.com>
2025-03-10 14:37:22 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,23 +1,38 @@
 import sys
+import shutil
 
+builtin_cmds = {}
+
+def command(func):
+    builtin_cmds[func.__name__.split("_")[1]] = func
+    return func
+
+@command
+def shell_exit(args):
+    exit(int(args[0]))
+
+@command
+def shell_echo(args):
+    sys.stdout.write(" ".join(args) + "\n")
+
+@command
+def shell_type(args):
+    if args[0] in builtin_cmds:
+        sys.stdout.write(f"{args[0]} is a shell builtin")
+    elif path := shutil.which(args[0]):
+        sys.stdout.write(f"{args[0]} is {path}\n")
+    else:
+        sys.stdout.write(f"{args[0]}: not found\n")
 
 def main():
     while True:
-        # sys.stdout.write("$ ")
-        command = input("$ ")
-
-        match command.split():
-            case ["type", arg] if arg in ["echo", "exit", "type"]:
-                print(f"{arg} is a shell builtin")
-            case ["type", arg] if arg not in ["echo", "exit", "type"]:
-                print(f"{arg}: not found")
-            case ["exit", "0"]:
-                exit()
-            case ["echo", *args]:
-                print(*args)
-            case _:
-                print(f"{command}: command not found")
-                            
+        sys.stdout.write("$ ")
+        command = input("")
+        cmd, *args = command.split()
+        if cmd in builtin_cmds:
+            builtin_cmds[cmd](args)
+        else:
+            sys.stdout.write(f"{cmd}: command not found\n")
     return
 
 
