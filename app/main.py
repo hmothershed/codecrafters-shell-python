cae3b190c8fefa2754a6f6254fba90ecd16e607c
hmothershed <harmony.mothershed@icloud.com>
2025-03-10 15:23:22 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,59 +1,47 @@
 import sys
-import shutil
-import subprocess
 import os
 
-builtin_cmds = {}
-
-def command(func):
-    builtin_cmds[func.__name__.split("_")[1]] = func
-    return func
-
-@command
-def shell_exit(args):
-    exit(int(args[0]))
-
-@command
-def shell_echo(args):
-    sys.stdout.write(" ".join(args) + "\n")
-
-@command
-def shell_type(args):
-    if args[0] in builtin_cmds:
-        sys.stdout.write(f"{args[0]} is a shell builtin\n")
-    elif path := shutil.which(args[0]):
-        # get the base name of the executable file without the full path
-        program_name = os.path.basename(path)
-        sys.stdout.write(f"{args[0]} is {program_name}\n")
+supported_commands = ["echo", "exit", "type"]
+ 
+def find_command_in_path(command):
+    path_dirs = os.environ.get("PATH", "").split(os.pathsep)
+    for dir in path_dirs:
+        path = os.path.join(dir, command)
+        if os.path.isfile(path) and os.access(path, os.X_OK):
+            return path
+    return None
+ 
+def echo(data: list):
+    print(" ".join(data))
+ 
+def exit_cmd(data: list):
+    exit(int(data[0]))
+ 
+def type_cmd(data: list):
+    if data[0] in supported_commands:
+        print(f"{data[0]} is a shell builtin")
+    elif path := find_command_in_path(data[0]):
+        print(f"{data[0]} is {path}")
     else:
-        sys.stdout.write(f"{args[0]}: not found\n")
-
-def run_external_program(cmd, args):
-    # Find the path of the executable
-    path = shutil.which(cmd)
-    if path:
-        try:
-            # Run the external program with arguments
-            subprocess.run([path] + args)
-        except Exception as e:
-            sys.stdout.write(f"Error running {cmd}: {e}\n")
-    else:
-        sys.stdout.write(f"{cmd}: command not found\n")
-
+        print(f"{data[0]} not found")
+ 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input("")
-        if not command.strip():
-            continue
+        sys.stdout.flush()
+         # Wait for user input
+        command, *data = input().split(" ")
+        if command == "exit":
+            exit_cmd(data)
+        elif command == "echo":
+            echo(data)
+        elif command == "type":
+            type_cmd(data)
+        elif path := find_command_in_path(command):
 
-        cmd, *args = command.split()
-        if cmd in builtin_cmds:
-            builtin_cmds[cmd](args)
+            os.system(f"{path} {' '.join(data)}")
         else:
-            run_external_program(cmd, args)
-    return
-
-
+            print(f"{command}: command not found")
+ 
 if __name__ == "__main__":
     main()
