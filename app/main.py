b23e65b8e784fb67d34650b4dd76e55d013a1a2b
hmothershed <harmony.mothershed@icloud.com>
2025-03-10 15:17:53 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import shutil
 import subprocess
+import os
 
 builtin_cmds = {}
 
@@ -21,7 +22,9 @@ def shell_type(args):
     if args[0] in builtin_cmds:
         sys.stdout.write(f"{args[0]} is a shell builtin\n")
     elif path := shutil.which(args[0]):
-        sys.stdout.write(f"{args[0]} is {path}\n")
+        # get the base name of the executable file without the full path
+        program_name = os.path.basename(path)
+        sys.stdout.write(f"{args[0]} is {program_name}\n")
     else:
         sys.stdout.write(f"{args[0]}: not found\n")
 
@@ -30,7 +33,9 @@ def run_external_program(cmd, args):
     path = shutil.which(cmd)
     if path:
         try:
+            program_name = os.path.basename(path)
             # Run the external program with arguments
+            sys.stdout.write(f"Running: {program_name} {' '.join(args)}\n")
             subprocess.run([path] + args)
         except Exception as e:
             sys.stdout.write(f"Error running {cmd}: {e}\n")
